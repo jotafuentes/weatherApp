5d5836fe5ade31c1dd241ebfd293ee57c1c3606a
jotafuentes <joseluisfuentes17@gmail.com>
2024-02-18 19:01:29 +0100

    fix API problems

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,5 +1,5 @@
 export function dataWeather (lat, lon, endpoint) {
-  const API_KEY = import.meta.env.VITE_API_KEY
+  const API_KEY = import.meta.env.WEATHER_API_KEY
 
   return fetch(`${endpoint}?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
     .then(response => response.json())
